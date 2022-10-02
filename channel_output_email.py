f22892ef9e07d7a948fe8987efad1b18b45a8db8
JStyle21 <unfold1911@gmail.com>
2022-10-02 04:02:24 +0300

    email template fix?

--- channel_output_email.py
+++ channel_output_email.py
@@ -170,7 +170,6 @@ class EmailOutputChannel(OutputChannel):
                         "To": [
                             {
                                 "Email": canarydrop['alert_email_recipient'],
-                                "Name": ""
                             }
                         ],
                         "Subject": msg['subject'],
